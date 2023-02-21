36a870140be68760d2daf4879907b775fbf55154
Stunning Hub <60594587+stunninghub@users.noreply.github.com>
2023-02-22 00:22:25 +0530

    CSS Fixes

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,53 +1,70 @@
-@extends('0000000000')
+@extends('base')
 @section('title')
 Posts
 @endsection
 @section('content')
-<form action="" id="save_post_form">
-    @csrf
-    <div class="container">
-        <div class="form-group">
-            <select name="select" id="select_id">
-                <option value="">Add new</option>
-                @foreach($all_posts as $post)
-                <option value="{{ $post->id }}">{{ $post->id }} => {{ $post->name }}</option>
-                @endforeach
-            </select>
-        </div><br>
-        <div class="form-group">
-            <label for="name">Name:</label>
-            <input type="text" name="name" id="name">
-        </div><br>
-        <div class="form-group">
-            <label for="name">description:</label>
-            <input type="text" name="description" id="description">
-        </div><br>
-        <div class="form-group">
-            <button type="submit" name="save" id="save">Save</button>
+<div class="posts_form_wrap">
+    <div class="posts_form_left">
+        <div class="heading">
+            <h2>Posts</h2>
         </div>
+        <form action="" id="save_post_form">
+            @csrf
+            <div class="container">
+                <div class="form-group">
+                    <label for="select_id">Action:</label>
+                    <select name="select" id="select_id">
+                        <option value="">Add new</option>
+                        @foreach($all_posts as $post)
+                        <option value="{{ $post->id }}">{{ $post->id }} => {{ $post->name }}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label for="name">Name:</label>
+                    <input type="text" name="name" id="name">
+                </div>
+                <div class="form-group">
+                    <label for="name">description:</label>
+                    <input type="text" name="description" id="description">
+                </div>
+                <div class="form-group">
+                    <button type="submit" name="save" id="save">Save</button>
+                </div>
+            </div>
+        </form>
     </div>
-</form>
-<table class="post_data_rows">
-    <thead>
-        <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Description</th>
-            <th>Action</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach($all_posts as $post)
-        <tr>
-            <td>{{ $post->id }}</td>
-            <td>{{ $post->name }}</td>
-            <td>{{ $post->description }}</td>
-            <td><button class="remove_post" data-id="{{ $post->id }}">&times;</button></td>
-        </tr>
-        @endforeach
-    </tbody>
-</table>
+    <div class="posts_form_right">
+        <table class="post_data_rows">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Description</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($all_posts as $post)
+                <tr>
+                    <td>{{ $post->id }}</td>
+                    <td>{{ $post->name }}</td>
+                    <td>{{ $post->description }}</td>
+                    <td><button class="remove_post" data-id="{{ $post->id }}">&times;</button></td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
 <script>
+    $('#save_post_form input').on('keyup', function() {
+        $('#save_post_form button[type=submit]').css({
+            opacity: 1,
+            pointerEvents: 'all',
+        });
+        $('#save_post_form button[type=submit]').text('Save');
+    })
     $(document).on('submit', '#save_post_form', function(e) {
         e.preventDefault();
         let this_form = $(this);
@@ -58,12 +75,24 @@ Posts
             data: post_data,
             cache: false,
             processData: false,
-            contentType: false
+            contentType: false,
+            beforeSend: function() {
+                this_form.find('button[type=submit]').css({
+                    opacity: 0.6,
+                    pointerEvents: 'none',
+                });
+                this_form.find('button[type=submit]').text('Saving..');
+            }
         }).done((res) => {
             console.log(res);
             let post_id_val = res.post_id;
-            if ($('#select_id') == "") {
+            if ($('#select_id').val() == "") {
                 this_form.get(0).reset();
+                this_form.find('button[type=submit]').css({
+                    opacity: 1,
+                    pointerEvents: 'All',
+                });
+                this_form.find('button[type=submit]').text('Saved');
                 $('#select_id').append(`<option value="${post_id_val}">${post_id_val} =>. ${res.name}</option>`);
                 $('.post_data_rows tbody').append(`
             <tr>
